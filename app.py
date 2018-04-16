d69f15d2105570e001abfcb49fdaf45671988912
sarojaerabelli <saroja.erabelli@gmail.com>
2018-04-16 14:48:47 -0400

    Added signatures for uploaded photo and merged.

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request
 import sqlite3
 import os
+from base64 import b64encode, b64decode
+import time
 
-from generate_key import generate_RSA_keys
+from generate_key import generate_RSA_keys, sign
 app = Flask(__name__)
 
 visits_db = 'db/users.db'
 
-UPLOAD_FOLDER = '/uploads'
+UPLOAD_FOLDER = os.path.basename('uploads')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/")
@@ -23,23 +25,26 @@ def generateKey():
     table1 = c.execute('''SELECT * from user_table;''').fetchall()
     conn.commit() #commit commands
     conn.close()
-    #return str(table1)
     return render_template('index.html', public_key=public_key)
-
-@app.route('/showSignUp')
-def showSignUp():
-	# f = open(visits_db, "r")
-	# print(f)
-	# return "yes"
 	
-	return str(table1)
-	
-	# return "here"
-	
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
-                               filename)
+@app.route('/addUser', methods=['GET', 'POST'])
+def addUser():
+    conn = sqlite3.connect(visits_db)
+    c = conn.cursor()
+    c.execute('''INSERT into user_table (first_name, last_name, dob, country, address, public_key) VALUES (?, ?, ?, ?, ?, ?);''',(request.form['inputFirstName'], request.form['inputLastName'], request.form['inputDOB'], request.form['inputCountry'], request.form['inputAddress'], request.form['inputPublicKey'])) #with time
+    table1 = c.execute('''SELECT * from user_table;''').fetchall()
+    conn.commit() #commit commands
+    conn.close()
+    return str(table1)
+    
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    file = request.files['datafile']
+    f = os.path.join(app.config['UPLOAD_FOLDER'], str(file.filename))
+    file.save(f)
+    f = open(f, 'rb')
+    signature = sign(b64encode(f.read() + str(time.time())))
+    return render_template('index.html', signature=signature)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=1025)
--- generate_key.py
+++ generate_key.py
@@ -1,6 +1,13 @@
 """Code for generating public and private key pair for RSA."""
 
 from Crypto.PublicKey import RSA
+from Crypto.Signature import PKCS1_v1_5 
+from Crypto.Hash import SHA256
+from base64 import b64encode, b64decode
+
+government_key = RSA.generate(2048, e=65537) 
+government_public_key = government_key.publickey().exportKey("PEM") 
+government_private_key = government_key.exportKey("PEM")
 
 def generate_RSA_keys(bits=2048):
     """Generate RSA private and public key pair with exponent 65537.
@@ -15,3 +22,12 @@ def generate_RSA_keys(bits=2048):
     public_key = new_key.publickey().exportKey("PEM") 
     private_key = new_key.exportKey("PEM") 
     return private_key, public_key
+
+def sign(data):
+    rsakey = RSA.importKey(government_private_key) 
+    signer = PKCS1_v1_5.new(rsakey) 
+    digest = SHA256.new() 
+    # It's being assumed the data is base64 encoded, so it's decoded before updating the digest 
+    digest.update(b64decode(data)) 
+    sign = signer.sign(digest) 
+    return b64encode(sign)
